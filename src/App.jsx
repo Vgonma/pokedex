89da56660fa75552aa8e6947683c71efb88af1ac
Vgonma <victorgmassimi.microverse@gmail.com>
2023-02-06 12:15:09 -0600

    File structure and routes setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route} from 'react-router-dom'
+import './App.css'
+import Details from './pages/Details'
+import Home from './pages/Home'
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<Details />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
