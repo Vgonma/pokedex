9224d55f2b27c820b8179453f8e529e600ebdb3a
Vgonma <victorgmassimi.microverse@gmail.com>
2023-02-06 18:19:38 -0600

    Fetch data and pagination

--- src/redux/pokedexSlice.js
+++ src/redux/pokedexSlice.js
@@ -2,10 +2,23 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 // Actions
 const GET_DATA = 'getPokemon';
+const GET_NEXT = 'getNext';
 
 // Aditional Action Creators
-export const getData = createAsyncThunk(GET_DATA, async () => (
-  fetch('https://pokeapi.co/api/v2/pokemon/')
+export const getData = createAsyncThunk(GET_DATA, async (page = 0) => (
+  fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${20 * page}`)
+    .then((response) => response.json())
+    .then((data) => ( // Data contains the name and url for each individual pokemon.
+      Promise.all(
+        // Fetch each individual pokemon data and map it.
+        data.results.map(async (pokemon) => ((await fetch(pokemon.url)).json())),
+        // Finally return an object like so: {next: <nextPageUrl>, [list, of, pokemon]}
+      ).then((pokemonList) => ({ total: data.count, next: data.next, pokemon: [...pokemonList] }))
+    ))
+));
+
+export const getNextPage = createAsyncThunk(GET_NEXT, async (nextUrl) => (
+  fetch(nextUrl)
     .then((response) => response.json())
     .then((data) => ( // Data contains the name and url for each individual pokemon.
       Promise.all(
@@ -16,12 +29,10 @@ export const getData = createAsyncThunk(GET_DATA, async () => (
     ))
 ));
 
-export
-
 const initialState = {
   loading: false,
   pokemon: [],
-  next: '',
+  next: 0,
 };
 
 const geSlice = createSlice({
@@ -30,10 +41,12 @@ const geSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(getData.fulfilled, (state, action) => {
+        if (!action.payload) return;
         const prevState = state;
         prevState.loading = false;
         prevState.pokemon = [...action.payload.pokemon];
         prevState.next = action.payload.next;
+        prevState.total = action.payload.total;
       })
       .addCase(getData.pending, (state) => {
         const prevState = state;
@@ -42,6 +55,12 @@ const geSlice = createSlice({
       .addCase(getData.rejected, (state) => {
         const prevState = state;
         prevState.loading = false;
+      })
+      .addCase(getNextPage.fulfilled, (state, action) => {
+        const prevState = state;
+        prevState.loading = false;
+        prevState.pokemon = [...state.pokemon, ...action.payload.pokemon];
+        prevState.next = action.payload.next;
       });
   },
 });
